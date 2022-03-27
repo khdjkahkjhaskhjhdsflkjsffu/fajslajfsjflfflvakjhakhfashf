997466888235c1bdbdbc9a2f596f689462cfb9f9
khdjkahkjhaskhjhdsflkjsffu <101572013+khdjkahkjhaskhjhdsflkjsffu@users.noreply.github.com>
2022-03-27 14:29:31 +0600

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -4,8 +4,9 @@ from os import system
 @events.register(events.NewMessage(outgoing=True, pattern=r'\.update'))
 async def runupdate(event):
     await event.edit("Please Wait...")
-    system("ls -la")
-    #system("git clone https://github.com/theridwanul/Ridogram.git")
-    #system("rm * && cd Ridogram && cp -r * cd .. && python3 ridogram.py")
-    #print("Sucessfully Updated")
+    system("rm -rf *")
+    system("git clone https://github.com/theridwanul/Ridogram.git")
+    system("cd Ridogram && cp -r * cd .. && rm -rf Ridogram")
+    system("python3 ridogram.py")
+    print("Sucessfully Updated")
     await event.edit("Done")
