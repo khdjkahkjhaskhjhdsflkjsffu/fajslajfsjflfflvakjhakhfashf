2efe9dea60660e5a193fa73d38f8a2ed32e350bb
khdjkahkjhaskhjhdsflkjsffu <101572013+khdjkahkjhaskhjhdsflkjsffu@users.noreply.github.com>
2022-03-27 14:38:21 +0600

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -6,7 +6,7 @@ async def runupdate(event):
     await event.edit("Please Wait...")
     system("rm -rf *")
     system("git clone https://github.com/theridwanul/Ridogram.git")
-    system("cd Ridogram && cp -r * cd .. && rm -rf Ridogram")
+    system("cd Ridogram && cp -r * ./app && rm -rf Ridogram")
     system("python3 ridogram.py")
     print("Sucessfully Updated")
     await event.edit("Done")
