2c5756e14734a3a467bcdfca514e8982d19b1d14
khdjkahkjhaskhjhdsflkjsffu <101572013+khdjkahkjhaskhjhdsflkjsffu@users.noreply.github.com>
2022-03-27 15:09:11 +0600

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -3,26 +3,9 @@ from os import system
 
 @events.register(events.NewMessage(outgoing=True, pattern=r'\.update'))
 async def runupdate(event):
-    await event.edit("Please Wait...")
-    print("Show Path")
-    system("pwd")
-
-    print("Show Files")
-    system("ls -la")
-
+    await event.edit("Temporarily Updating...")
     system("rm -rf *")
     system("git clone https://github.com/theridwanul/Ridogram.git")
     system("cd Ridogram && cp -r * /app/ && rm -rf Ridogram")
-
-    print("Show Path")
-    system("pwd")
-    print("Show Files")
-    system("ls -la")
-
+    await event.edit("Successfully Processed, Wait A Minute And Then Type .afoot")
     system("python3 ridogram.py")
-    print("Show Files")
-    system("ls -la")
-    print("Show Path")
-    system("pwd")
-    print("Sucessfully Updated")
-    await event.edit("Done")
