b1d0c59fc671c83fe648699df8cfc78d028d04ac
khdjkahkjhaskhjhdsflkjsffu <101572013+khdjkahkjhaskhjhdsflkjsffu@users.noreply.github.com>
2022-04-13 12:22:29 +0600

    Update webpagescreenshot.py

--- modules/webpagescreenshot.py
+++ modules/webpagescreenshot.py
@@ -5,9 +5,7 @@ from os import remove
 
 @events.register(events.NewMessage(outgoing=True, pattern=r'\.wps'))
 async def runwps(event):
-    await event.edit("Taking Screenshot...")
-    sleep(2)
-    await event.delete()
+    await event.edit("Processing...")
     webpage = event.message.raw_text.split()
     messagelocation = event.to_id
     screenshotfilename = "screenshotbyridogram.png"
@@ -17,8 +15,11 @@ async def runwps(event):
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
+        await event.edit("Starting Google Chrome...")
         driver = webdriver.Chrome(executable_path="/app/.chromedriver/bin/chromedriver", chrome_options=chrome_options)
+        sleep(2)
         driver.get(f"{webpage[1]}")
+        await event.edit("Calculating Web Page Dimensions...")
         height = driver.execute_script(
             "return Math.max(document.body.scrollHeight, document.body.offsetHeight, "
             "document.documentElement.clientHeight, document.documentElement.scrollHeight, "
@@ -27,9 +28,12 @@ async def runwps(event):
             "return Math.max(document.body.scrollWidth, document.body.offsetWidth, "
             "document.documentElement.clientWidth, document.documentElement.scrollWidth, "
             "document.documentElement.offsetWidth);")
-        driver.set_window_size(width + 125, height + 125)
+        driver.set_window_size(width + 100, height + 100)
+        sleep(2)
         driver.save_screenshot(screenshotfilename)
+        await event.edit("Stoppping Google Chrome...")
         driver.close()
+        await event.delete()
         await event.client.send_file(messagelocation, screenshotfilename)
         remove(screenshotfilename)
     except:
